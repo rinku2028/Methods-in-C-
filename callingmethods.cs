b8df8f1b3538b05a108fc87f04dcf106e78bcce3
rinku2028 <40994882+rinku2028@users.noreply.github.com>
2020-02-05 19:11:45 +0545

    Calling method in C#

---
+++ callingmethods.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+
+namespace methods
+{
+    class callingmethods
+    {
+        public int FindMaxNum(int num1,int num2)
+        {
+            //declare local variable
+            int result;
+            if (num1 > num2)
+            {
+                result = num1;
+            }
+            else
+            {
+                result = num2;
+            }
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            callingmethods cc = new callingmethods();
+            int x, y, res;
+            x = 200;
+            y = 500;
+            res = cc.FindMaxNum(x, y);
+            Console.WriteLine("Max number is:{0}",res);
+            Console.ReadLine();
+        }
+    }
+}
