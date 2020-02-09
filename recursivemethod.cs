37a706654b9545decee475f69a97c101abd31902
rinku2028 <40994882+rinku2028@users.noreply.github.com>
2020-02-09 21:52:17 +0545

    Recursive Method

---
+++ recursivemethod.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace methods
+{
+    class recursivemethod
+    {
+        public static double Factorial(int number)
+        {
+            if (number == 0)
+                return 1;
+            return number * Factorial(number - 1);//Recursive call  
+
+        }
+        static void Main(string []args)
+        {
+            Console.WriteLine("Enter number:");
+
+            //Read number input by the user    
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\n");
+
+            //Invoke  the static method  Factorial 
+            double factorial = Factorial(number);
+
+            //print the factorial result    
+            Console.WriteLine("Factorial of" + number + "=" + factorial.ToString());
+            Console.ReadLine();
+        }
+    }
+}
